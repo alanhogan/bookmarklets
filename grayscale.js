9f1e6371ef2451af4f5506f075b1bb175a3689ab
Alan Hogan <contact@alanhogan.com>
2016-09-30 16:24:01 -0700

    Add grayscale bookmarklet

---
+++ grayscale.js
@@ -0,0 +1,6 @@
+(function(){
+  var s=(document.getElementsByTagName('head')[0]||document.body).appendChild(document.createElement('style'));
+  var t=document.createTextNode('body {  filter: grayscale(100%) !important;  -webkit-filter: grayscale(100%) !important;}');
+  s.appendChild(t);
+}
+)();
