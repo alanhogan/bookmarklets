000dbbe988720b6cfee3561a66ef9f98dfc72b1a
Alan Hogan <contact@alanhogan.com>
2017-03-21 11:57:38 -0700

    Fix detection of fonts with periods in the name

--- font-stack-guess.js
+++ font-stack-guess.js
@@ -5,6 +5,8 @@
 // Some code has been previously converted by the Babel REPL to survive
 // running in IE11. Avoid ES6 or transpile before committing updates.
 
+var versionStr = '1.0';
+
 var Detector = function Detector() {
   var baseFonts = ['monospace', 'sans-serif', 'serif'];
 
@@ -30,7 +32,7 @@ var Detector = function Detector() {
   function detect(font) {
     var detected = false;
     for (var index in baseFonts) {
-      s.style.fontFamily = font + ',' + baseFonts[index];
+      s.style.fontFamily = '\'' + font + '\', ' + baseFonts[index];
       h.appendChild(s);
       var matched = s.offsetWidth != defaultWidth[baseFonts[index]] || s.offsetHeight != defaultHeight[baseFonts[index]];
       h.removeChild(s);
@@ -44,7 +46,7 @@ var Detector = function Detector() {
 
 var d = new Detector();
 
-function guessBodyFont() {
+function guessBodyFont(fullReport) {
   var body = document.getElementsByTagName('body')[0];
   var stack = body ? window.getComputedStyle(body).fontFamily : '';
   var fonts = stack.split(/\s*,\s*/).map(function (font) {
@@ -63,10 +65,30 @@ function guessBodyFont() {
   });
 
   if (probableFont) {
-    alert("BODY element is probably using the font family or keyword: \n" + probableFont + "\n\n" + "All matching font names or keywords in the stack: \n" + availableFonts.join("\n") + "\n\n" + "Unavailable fonts: \n" + (unavailableFonts.length > 0 ? unavailableFonts.join("\n") : '(None)') + "\n\n" + "Font stack as reported by getComputedStyle: \n" + stack + "\n\nNote cautions and update this bookmarklet at https://alanhogan.com/bookmarklets#font-stack-guess");
+    var reportStr = "BODY element is probably using the font family or keyword: \n" + probableFont;
+    if (fullReport) {
+      reportStr = reportStr + "\n\n" + "All matching font names or keywords in the stack: \n" + availableFonts.join("\n") + "\n\n" + "Unavailable fonts: \n" + (unavailableFonts.length > 0 ? unavailableFonts.join("\n") : '(None)') + "\n\n" + "Font stack as reported by getComputedStyle: \n" + stack;
+      reportStr = reportStr + "\n\nOnly Latins characters are tested, so detection will fail for Asian fonts, emojis, and system fonts. ";
+    } else {
+      reportStr = reportStr + "\n\n"
+    }
+
+    reportStr = reportStr + "More info and potentially a newer version of this bookmarklet are available at "
+      + "https://alanhogan.com/bookmarklets"
+      + (fullReport ? '#font-stack-full' : '#font-stack-guess');
+    
+    reportStr = reportStr + "\n\This is version " + versionStr;
+    
+    alert(reportStr);
   } else {
     alert('Did not detect a font set for body');
   }
 }
 
-guessBodyFont();
+function fullBodyFontStackReport() {
+  guessBodyFont(true);
+}
+
+// Uncomment ONE of these to generate the simple vs full version of the bookmarklet.
+// guessBodyFont();
+fullBodyFontStackReport();
