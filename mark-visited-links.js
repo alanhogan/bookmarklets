67912792bb1400d0e1f0b8181d197dbeffeb0fc2
Alan Hogan <contact@alanhogan.com>
2019-01-28 11:46:53 -0800

    Darken colors in mark-visited-links

--- mark-visited-links.js
+++ mark-visited-links.js
@@ -1,4 +1,4 @@
-var version = '1.0';
+var version = '1.1';
 var name = 'com.alanhogan.bookmarklets.mark-visited.' + version;
 
 var extant = document.querySelector('style[data-via-bookmarklet="'+name+'"]');
@@ -9,7 +9,7 @@ if (extant !== null) {
 
 var css = `
 a:link {
-  color: rgb(5%, 35%, 90%) !important;
+  color: rgb(5%, 27%, 74%) !important
   text-shadow: 0 0.06em 0.3em rgba(100%, 100%, 100%, 1),
     -1px 1px 1px rgba(100%, 100%, 100%, 1),
     1px 1px 1px rgba(104%, 100%, 100%, 1),
@@ -17,7 +17,7 @@ a:link {
     1px -1px 1px rgba(100%, 100%, 100%, 1)  !important;
 }
 a:visited {
-  color: rgb(70%, 2%, 69%) !important;
+  color: rgb(60%, 2%, 53%) !important
 }
 `;
 
