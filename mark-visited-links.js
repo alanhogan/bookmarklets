cac43c0d378cb991e6aa57c343d1d2aa0cb45302
Alan Hogan <contact@alanhogan.com>
2019-01-28 16:45:11 -0800

    Apply style more deeply

--- mark-visited-links.js
+++ mark-visited-links.js
@@ -1,4 +1,4 @@
-var version = '1.1';
+var version = '1.2';
 var name = 'com.alanhogan.bookmarklets.mark-visited.' + version;
 
 var extant = document.querySelector('style[data-via-bookmarklet="'+name+'"]');
@@ -8,16 +8,20 @@ if (extant !== null) {
 }
 
 var css = `
-a:link {
-  color: rgb(5%, 27%, 74%) !important
+a:link{
   text-shadow: 0 0.06em 0.3em rgba(100%, 100%, 100%, 1),
     -1px 1px 1px rgba(100%, 100%, 100%, 1),
     1px 1px 1px rgba(104%, 100%, 100%, 1),
     -1px -1px 1px rgba(100%, 100%, 100%, 1), 
     1px -1px 1px rgba(100%, 100%, 100%, 1)  !important;
 }
-a:visited {
-  color: rgb(60%, 2%, 53%) !important
+a:link,
+a:link * {
+  color: rgb(5%, 27%, 74%) !important;
+}
+a:visited,
+a:visited * {
+  color: rgb(60%, 2%, 53%) !important;
 }
 `;
 
