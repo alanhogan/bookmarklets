dd32d91828a8d670ef72060a0b6cbf221360d836
Alan Hogan <contact@alanhogan.com>
2019-03-08 14:58:09 -0800

    Run prettier on text selection bml

--- enable-text-selection.js
+++ enable-text-selection.js
@@ -9,41 +9,49 @@
  *
  * Crunch at https://alanhogan.github.io/bookmarkleter/
  */
+
 function allowTextSelection() {
-    window.console && console.log('allowTextSelection');
+  window.console && console.log("allowTextSelection");
 
-    //add styles that enable text selection
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML =
-        '*,p,div{user-select:text !important;-moz-user-select:text !important;-webkit-user-select:text !important;}';
+  //add styles that enable text selection
+  var style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = `*, p, div {
+    user-select: text !important;
+    -moz-user-select: text !important;
+    -webkit-user-select:text !important;}
+  `;
 
-    document.head.appendChild(style);
+  document.head.appendChild(style);
 
-    //Put all of <body> children in a collection
-    //Use getElementsByTagName because it has better compatibility (it's older) than querySelectorAll('*')
-    var elArray = document.body.getElementsByTagName('*');
+  //Put all of <body> children in a collection
+  //Use getElementsByTagName because it has better compatibility (it's older) than querySelectorAll('*')
+  var elArray = document.body.getElementsByTagName("*");
 
-    //allow mouse events typically involved in selection
-    for(var i=0; i<elArray.length; i++){
-        var el = elArray[i];
-        el.onselectstart = el.ondragstart = el.ondrag = el.oncontextmenu = el.onmousedown
-            = el.onmouseup = function(){return true};
+  //allow mouse events typically involved in selection
+  for (var i = 0; i < elArray.length; i++) {
+    var el = elArray[i];
+    el.onselectstart = el.ondragstart = el.ondrag = el.oncontextmenu = el.onmousedown = el.onmouseup = function() {
+      return true;
+    };
 
-        //special processing for text-style <input> elements
-        if(el instanceof HTMLInputElement &&
-            ['text','password','email','number','tel','url'].indexOf(el.type.toLowerCase()) > -1)
-        {
-            //enable text inputs (to defeat an easy way to block selection by setting input's 'disabled' attribute)
-            el.removeAttribute('disabled');
+    //special processing for text-style <input> elements
+    if (
+      el instanceof HTMLInputElement &&
+      ["text", "password", "email", "number", "tel", "url"].indexOf(
+        el.type.toLowerCase()
+      ) > -1
+    ) {
+      //enable text inputs (to defeat an easy way to block selection by setting input's 'disabled' attribute)
+      el.removeAttribute("disabled");
 
-            //counteract any listener that would block copy&paste keyboard shortcuts. (I can't figure out yet why
-            // although this works on the first text input in text-selection-demo.html, it doesn't work on the 2nd
-            el.onkeydown = el.onkeyup = function(){return true};
-        }
-    }       
-
+      //counteract any listener that would block copy&paste keyboard shortcuts. (I can't figure out yet why
+      // although this works on the first text input in text-selection-demo.html, it doesn't work on the 2nd
+      el.onkeydown = el.onkeyup = function() {
+        return true;
+      };
+    }
+  }
 }
 
 allowTextSelection();
-
