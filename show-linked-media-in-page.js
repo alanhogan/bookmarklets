920d03f25cb57bec6e2915a013e073446d046733
Alan Hogan <contact@alanhogan.com>
2020-12-19 19:19:03 -0800

    Match more formats & case

--- show-linked-media-in-page.js
+++ show-linked-media-in-page.js
@@ -1,9 +1,9 @@
 (function() {
-	const linkedImages = document.querySelectorAll('a[href$=".gif"], a[href$=".jpeg"], a[href$=".jpg"], a[href$=".png"], a[href$=".apng"], a[href$=".webp"]');
+	const linkedImages = document.querySelectorAll('a[href$=".gif" i], a[href$=".jpeg" i], a[href$=".jpg" i], a[href$=".png" i], a[href$=".apng" i], a[href$=".webp" i], a[href$=".avif" i], a[href$=".svg" i]');
 	linkedImages.forEach(function(a) {
 		a.insertAdjacentHTML('afterend', '<div><img alt="" src="'+a.href.replaceAll('"','&quot;')+'" style="max-width:100%" /></div>')
 	});
-	const linkedVideos = document.querySelectorAll('a[href$=".mov"], a[href$=".mp4"], a[href$=".avi"], a[href$=".webm"], a[href$=".wmv"]');
+	const linkedVideos = document.querySelectorAll('a[href$=".mov" i], a[href$=".mp4" i], a[href$=".avi" i], a[href$=".webm" i], a[href$=".wmv" i], a[href$=".flv" i] a[href$=".mkv" i]');
 	linkedVideos.forEach(function(a) {
 		a.insertAdjacentHTML('afterend', '<div><video controls src="'+a.href.replaceAll('"','&quot;')+'" style="max-width:100%"></video></div>')
 	});
