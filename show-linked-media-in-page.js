1d5624ce7d0915973996ce760fe7afc3076d1d38
Alan Hogan <contact@alanhogan.com>
2020-12-19 19:04:47 -0800

    Remove undefined class name from show-linked-media

--- show-linked-media-in-page.js
+++ show-linked-media-in-page.js
@@ -1,10 +1,10 @@
 (function() {
 	const linkedImages = document.querySelectorAll('a[href$=".gif"], a[href$=".jpeg"], a[href$=".jpg"], a[href$=".png"], a[href$=".apng"], a[href$=".webp"]');
 	linkedImages.forEach(function(a) {
-		a.insertAdjacentHTML('afterend', '<div class="large-picture-wrap"><img alt="" src="'+a.href.replaceAll('"','&quot;')+'" /></div>')
+		a.insertAdjacentHTML('afterend', '<div><img alt="" src="'+a.href.replaceAll('"','&quot;')+'" style="max-width:100%" /></div>')
 	});
 	const linkedVideos = document.querySelectorAll('a[href$=".mov"], a[href$=".mp4"], a[href$=".avi"], a[href$=".webm"], a[href$=".wmv"]');
 	linkedVideos.forEach(function(a) {
-		a.insertAdjacentHTML('afterend', '<div class="large-picture-wrap"><video controls src="'+a.href.replaceAll('"','&quot;')+'" style="max-width:100%"></video></div>')
+		a.insertAdjacentHTML('afterend', '<div><video controls src="'+a.href.replaceAll('"','&quot;')+'" style="max-width:100%"></video></div>')
 	});
 })();
