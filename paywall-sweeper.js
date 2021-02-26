9c8df52f5a2a8b82ab03ef6d8775280f518d8024
Alan Hogan <contact@alanhogan.com>
2021-02-25 18:10:17 -0800

    Create paywall-sweeper.js

---
+++ paywall-sweeper.js
@@ -0,0 +1,6 @@
+// For when certain sites show you an onerous paywall.
+// Compile with jQuery option on at https://alanhogan.github.io/bookmarkleter/
+// Version 0.1
+
+$('#graphics-paywall-overlay').remove();
+$('body, html').removeAttr('data-paywall-overlay-status');
