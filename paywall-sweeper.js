f2e6aa298aee2d35357c27c040f7b8ae04ec5b5a
Alan Hogan <contact@alanhogan.com>
2021-02-25 18:12:47 -0800

    Update paywall-sweeper.js

--- paywall-sweeper.js
+++ paywall-sweeper.js
@@ -1,6 +1,7 @@
 // For when certain sites show you an onerous paywall.
 // Compile with jQuery option on at https://alanhogan.github.io/bookmarkleter/
-// Version 0.1
+// Version 0.2
 
-$('#graphics-paywall-overlay').remove();
+// Rhymes with Doomblerg
+$('#graphics-paywall-overlay, .leaderboard-container, #banner-container').remove();
 $('body, html').removeAttr('data-paywall-overlay-status');
