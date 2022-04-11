3f6a69c6828c73a0e25ebfd9b6951f99400fbe81
Alan Hogan <git-commits@alanhogan.com>
2022-04-11 12:52:42 -0700

    Create funky-focus.js

---
+++ funky-focus.js
@@ -0,0 +1,49 @@
+const style = document.createElement('style');
+style.type = 'text/css';
+style.innerText = `
+
+@keyframes ffwiggle {
+    0%   { 
+        transform: translateX(0);
+    }
+    5%   { 
+        transform: translateX(-5px);
+    }
+    10%  { 
+        transform: translateX(5px);
+    }
+    15%  { 
+        transform: translateX(-5px);
+    }
+    20%  { 
+        transform: translateX(5px);
+    }
+    25%  { 
+        transform: translateX(-5px);
+    }
+    30%  { 
+        transform: translateX(5px);
+    }
+    35%  { 
+        transform: translateX(0);
+    }
+    100% { 
+        transform: translateX(0);
+    }
+}
+
+
+:focus {
+  /* FYI: Filters won't work on non-replaced inline boxes e.g. spans, links, etc without a change in the display property */
+  filter: hue-rotate(-24deg) brightness(1.1) !important;
+  outline: #eecc44a0 solid 5px !important;
+  outline-offset: -2px !important;
+  
+
+  animation-name: ffwiggle;
+  animation-duration: 6s;
+  animation-fill-mode: both;
+  animation-iteration-count: infinite;
+}
+`;
+document.head.appendChild(style);
