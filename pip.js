f581016ad0fd8380d569b35a3f3b57867a888813
Alan Hogan <git-commits@alanhogan.com>
2022-05-26 20:07:44 -0700

    Update pip.js

--- pip.js
+++ pip.js
@@ -1,4 +1,4 @@
-const VERSION = "1.0";
+const VERSION = "1.1";
 const messageSuffix = "\n\n--pip.js version v"+VERSION+"\nalanhogan.com/bookmarklets";
 
 if (!document.pictureInPictureEnabled) {
@@ -8,7 +8,7 @@ if (!document.pictureInPictureEnabled) {
 
 const videos = document.getElementsByTagName("video");
 if (videos.length < 1) {
-  alert("No HTML5 videos were detected on page!" + messageSuffix);
+  alert("No HTML5 videos were detected on page! Note: The current version of this script does not deal with iFrames" + (document.getElementsByTagName('iframe').length ? " and there is at least one on the page." : ", but there are none detected on this page." ) + "." + messageSuffix);
   return;
 }
 
@@ -24,8 +24,17 @@ for (let i = 0; i <videos.length; i++) {
   } else if (!curPipFound && document.pictureInPictureElement == videos[i]) {
     curPipFound = true;
     console.log('Found!' + messageSuffix);
-    if (i == videos.length - 1) { videos[0].requestPictureInPicture(); }
+    if (i == videos.length - 1) {
+      videos[0].requestPictureInPicture();
+      break; 
+    }
   } else if (!curPipFound) {
     console.log('not found yet...' + messageSuffix);
+  } else if (curPipFound) {
+    videos[i].requestPictureInPicture();
+    break;
   }
+}
+if (!curPipFound && document.pictureInPictureElement) {
+  document.exitPictureInPicture();
 }
