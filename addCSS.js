df18dca24ad987bccafc1b2ee080e0ee6593fdc3
Alan Hogan <git-commits@alanhogan.com>
2022-07-08 16:10:53 -0700

    Update addCSS.js

--- addCSS.js
+++ addCSS.js
@@ -1,9 +1,12 @@
 /* This should probably have ES6 to ES5 transpilation on when compiling to bookmarklet */
 
-const version = '1.0';
+/* Suggested bookmarklet names: "+CSS" or "Apply Custom Stylesheet" */
+
+const version = '1.1';
 
 const styles = prompt('Enter CSS here to add it to the current page.');
 
 const styleElement = (document.getElementsByTagName('head')[0]||document.body).appendChild(document.createElement('style'));
+styleElement.dataset.ahBookmarkletInserted = "1";
 const textNode = document.createTextNode(styles);
 styleElement.appendChild(textNode);
