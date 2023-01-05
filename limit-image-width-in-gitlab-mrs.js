1223fc743b95ce7fdb82975f8983f04785e15174
Alan Hogan <git-commits@alanhogan.com>
2023-01-04 20:56:29 -0800

    Shrink images in MR review comment textareas

--- limit-image-width-in-gitlab-mrs.js
+++ limit-image-width-in-gitlab-mrs.js
@@ -1,7 +1,7 @@
 // Find images in Markdown format in text boxes on GitLab, then convert to HTML format with a set width.
 // Note that GitLab does not allow `style="max-width:350px"` instead, hence `width` attr here.
 const el = document.querySelector(
-  "textarea[name='merge_request[description]'], textarea#issue-description, textarea#note-body, textarea#wiki_content"
+  "textarea[name='merge_request[description]'], textarea#issue-description, textarea#note-body, textarea#wiki_content, textarea#review-note-body"
 );
 
 el.value = el.value.replace(
